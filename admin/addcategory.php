2710c96f01a35f305cce91c1217c881bd0c388ee
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2017-10-12 22:23:12 +0200

    main category logic

--- admin/addcategory.php
+++ admin/addcategory.php
@@ -11,57 +11,58 @@ if(isset($_POST) & !empty($_POST)){
 		$description = mysqli_real_escape_string($connection, $_POST['description']);
 
 		$sql = "INSERT INTO category (name, description) VALUES ('$name', '$description')";
-
-	if(!mysqli_query($connection,$sql))
-	{
-		echo'Not Inserted';
+		$res = mysqli_query($connection, $sql);
+		if($res){
+			$smsg = "Sub Category Added Successfully";
+		}else{
+			$fmsg = "Failed to Add Sub Category";
+		}
 	}
-	else
-	{
-		echo 'Inserted';
-	}
-
-
-}
-
 ?>
 <?php include 'inc/header.php'; ?>
 <?php include 'inc/nav.php'; ?>
-<div class="container">	
-		<div class="col-md-6">
- 			<div class="panel panel-default">
-				<div class="panel-body">
-				 <form method="post">
-				 	<div class="row">
-				 		<div class="col-md-6">
-				 		<form class="form-horizontal" method="post" action="#">
-				 			<label>Category Name </label>
-				 			<input name="name" class="form-control" placeholder="Category Name" value="" type="text" required="">
-				 			<br>
-				 			<label>Category Description</label>
-				 			<textarea name="description" class="form-control' rows="3"></textarea>
-				 			<br>
-				 			<input type="submit" name="Add Category" class="btn btn-primary btn-lg">
-				 		</form>
-				 		</div>
-				 	</div>
+<div class="container">
+	<div class="col-md-6">
+		<div class="panel panel-default">
+		  <div class="panel-body">
+		  	<form method="post">
+				<div class="row">
+					<div class="col-md-6">
+					<?php if(isset($smsg)){ ?><div class="alert alert-success" role="alert"> <?php echo $smsg; ?> </div><?php } ?>
+					<?php if(isset($fmsg)){ ?><div class="alert alert-danger" role="alert"> <?php echo $fmsg; ?> </div><?php } ?>
+					<form class="form-horizontal" method="post" action="#">
+						<label>Category Name </label>
+						<input name="name" class="form-control" placeholder="Category Name" value="" type="text" required="">
+						<br>
+						<label>Category Description </label>
+						<textarea name="description" class="form-control" rows="3"></textarea>
+						<br>
+						<input type="submit" value="Add Category" class="btn btn-primary btn-lg">
+					</form>
+					</div>
+					<div class="col-md-6">
+						
+					</div>
+				</div>	
 
-
-				 	<br>
-				 	<div class="row">
-				 	<div class="col-md-3"></div>
-				 	</div>
-
-				 </form>
+				<br>
+				<div class="row"> 
+				<div class="col-md-3"></div>
 				</div>
-			</div>
+                                            
+			</form>
+		  </div>
 		</div>
+	</div>
+		
 		<div class="col-md-6">
 			<div class="panel panel-default">
 				<div class="panel-body">
 					<form method="post">
 						<div class="row">
 							<div class="col-md-6">
+							<?php if(isset($ssmsg)) { ?><div class="alert alert-succes" role="alert"> <?php echo $smsg; ?> </div><?php } ?>
+							<?php if(isset($sfmsg)) { ?><div class="alert alert-danger" role="alert"> <?php echo $sfmsg; ?> </div><?php } ?>
 							<form class="form-horizontal" method="post" action="#">
 									<label> Select Main Category Name </label>
 								<select class="form-control">
@@ -85,7 +86,7 @@ if(isset($_POST) & !empty($_POST)){
 					</form>	
 				</div>
 			</div>
-		</div>
+		</div><!--md-6 -->
 </div><!-- main container -->
 <?php include 'inc/footer.php'; ?> 
 
