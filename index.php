93e69abb902fb32c5fa0a89155b85e4e7b79e050
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2017-10-17 15:30:51 +0200

    newcommit

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 session_start();
 require('config/connect.php');
 
-if(!isset($_SESSION['email']) & empty($_SESSION['email'])){
-	header('location: login.php');
-}
+
+
 ?>
 <?php 
 define("TITLE", "Home Page");
